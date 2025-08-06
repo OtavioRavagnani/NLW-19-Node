5ec3dda755b1230aeaab0daf197c130664e78c26
OtavioRavagnani <otavioravagnani01@gmail.com>
2025-08-05 21:50:28 -0300

    Aula 2

--- Server/src/env.ts
+++ Server/src/env.ts
@@ -2,6 +2,8 @@ import { object, z } from 'zod'
 
 const envSchema = object({
   PORT: z.coerce.number().default(3333),
+  POSTGRES_URL: z.string().url(),
+  REDIS_URL: z.string().url(),
 })
 
 export const env = envSchema.parse(process.env)
