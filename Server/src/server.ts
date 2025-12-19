af0322ab85320af7558b0e900edb3fafb7d33aa3
OtavioRavagnani <otavioravagnani01@gmail.com>
2025-12-18 22:16:08 -0300

    3 rota get feita com problemas
    minuto da aula: 15:03

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -12,6 +12,7 @@ import { env } from './env'
 import { subscribeToEventRoute } from './routes/subscriber-to-event-route'
 import { accessToInviteLinkRoute } from './routes/access-invite-link-route'
 import { getSubscriberInviteClicksRoute } from './routes/get-subscriber-invites-clicks-route'
+import { getSubscriberInvitesCountRoute } from './routes/get-subscriber-invites-count-route'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -37,6 +38,7 @@ app.register(fastifySwaggerUi, {
 app.register(subscribeToEventRoute)
 app.register(accessToInviteLinkRoute)
 app.register(getSubscriberInviteClicksRoute)
+app.register(getSubscriberInvitesCountRoute)
 
 
 app.listen({ port: env.PORT }).then(() => {
